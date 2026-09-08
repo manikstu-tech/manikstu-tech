bb9d607ee105d515e92b84871326daafce3a53ed
CodeByAftab <aftabkhan11480@gmail.com>
2026-09-08 23:26:05 +0530

    feat: redirect /admin to api.manikstu.com

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -1,7 +1,17 @@
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
+import {NextRequest, NextResponse} from 'next/server';
 
-export default createMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(
+      new URL(request.nextUrl.pathname, 'https://api.manikstu.com'),
+    );
+  }
+  return handleI18nRouting(request);
+}
 
 export const config = {
   matcher: [
