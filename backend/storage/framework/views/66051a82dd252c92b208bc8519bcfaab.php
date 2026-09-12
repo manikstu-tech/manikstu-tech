392f0949f6c940d950d02f675574a5a7bd938e7e
BISWAJIT822 <biswajitbehera4200@gmail.com>
2026-09-12 16:22:11 +0530

    chore: remove leftover backend vendor files from frontend repository

--- backend/storage/framework/views/66051a82dd252c92b208bc8519bcfaab.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('title', 'Add Product'); ?>
-
-<?php $__env->startSection('content'); ?>
-<?php echo $__env->make('admin.products._form', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('admin.layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH E:\Manikstu Website\backend\resources\views/admin/products/create.blade.php ENDPATH**/ ?>
